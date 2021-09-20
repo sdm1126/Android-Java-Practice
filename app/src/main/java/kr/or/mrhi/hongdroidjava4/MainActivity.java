2e4336e0a0ad4da4194a9fff8353319bdd01e959
sdm1126 <sdm1126@naver.com>
2021-09-20 17:48:29 +0900

    first commit

---
+++ app/src/main/java/kr/or/mrhi/hongdroidjava4/MainActivity.java
@@ -0,0 +1,16 @@
+package kr.or.mrhi.hongdroidjava4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// Github
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
