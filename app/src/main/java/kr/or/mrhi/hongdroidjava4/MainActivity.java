eb91b14150f693689b80830d732f0c7603d6c611
sdm1126 <sdm1126@naver.com>
2021-09-20 18:24:22 +0900

    1. "연동으로 협업하기" 추가 2. "주석 추가" 추가

--- app/src/main/java/kr/or/mrhi/hongdroidjava4/MainActivity.java
+++ app/src/main/java/kr/or/mrhi/hongdroidjava4/MainActivity.java
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-// Github
+// Github 연동으로 협업하기
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        // 주석 추가
+    }
 }
