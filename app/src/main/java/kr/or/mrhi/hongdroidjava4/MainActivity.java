f6fc5bf17aa76d083195706e6573ae0a6d7e0b50
sdm1126 <sdm1126@naver.com>
2021-09-20 18:50:58 +0900

    버튼 생성

--- app/src/main/java/kr/or/mrhi/hongdroidjava4/MainActivity.java
+++ app/src/main/java/kr/or/mrhi/hongdroidjava4/MainActivity.java
@@ -3,16 +3,26 @@ package kr.or.mrhi.hongdroidjava4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 // Github 연동으로 협업하기
 public class MainActivity extends AppCompatActivity {
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 주석 추가
-        
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "버튼", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
